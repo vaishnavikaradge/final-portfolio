2548840a57b7f289bf82c82d09735593e5b74e80
vaishnavikaradge <vaishnavikaradge@gmail.com>
2023-07-04 01:50:27 +0530

    thisisportfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+//import './App.css';
+import React from "react";
+import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import ContactUs from "./routes/ContactUs";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/project" element={<Project/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contactus" element={<ContactUs/>} />
+    </Routes>
+    </>
   );
 }
+
 
 export default App;
